9fc73a43c1e7c4ea1bb43117f383fa5ef8742996
Christoph Nuetzel <christoph.nuetzel@futurice.com>
2024-04-05 12:32:23 +0200

    Add files via upload

---
+++ klarna.py
@@ -0,0 +1,8 @@
+BASIC_CUSTOMER_PROMPT = """I received a damaged package and the shop is not responding. 
+I need the article and I bought it now somewhere else. Can you help me? I am a {} at the age of 
+{}. My financial situation is {}. I shop {} per month. Adress me with the tone of voice 
+{}. Emphasize according to the credit score of {} the impact to the financial situation, but be 
+mindful and take person in considderation. Take the usual shopping items of {} into consideration of how 
+stressed the person might be and what information Klarna can give. Write a personal message. Be subtle with any 
+financial situation mention. It should make the perspn feel good and not judged or put into a segment. 
+They should feel understood."""
